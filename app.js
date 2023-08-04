71997b60d2aa8378f671b158d8e9b9cf10a9d3cb
Moustapha-Ibrahima-Ba <swe.miba@gmail.com>
2023-08-04 21:53:33 +0000

    Refactoring and update

--- app.js
+++ app.js
@@ -1,116 +1,132 @@
-// API <=> DATA we need to build the table dynamically
 const warriorsGames = [
-  {
-    awayTeam: {
-      team: "Golden State",
-      points: 119,
-      isWinner: true,
-    },
-    homeTeam: {
-      team: "Houston",
-      points: 106,
-      isWinner: false,
-    },
-  },
-  {
-    awayTeam: {
-      team: "Golden State",
-      points: 105,
-      isWinner: false,
-    },
-    homeTeam: {
-      team: "Houston",
-      points: 127,
-      isWinner: true,
-    },
-  },
-  {
-    homeTeam: {
-      team: "Golden State",
-      points: 126,
-      isWinner: true,
-    },
-    awayTeam: {
-      team: "Houston",
-      points: 85,
-      isWinner: false,
-    },
-  },
-  {
-    homeTeam: {
-      team: "Golden State",
-      points: 92,
-      isWinner: false,
-    },
-    awayTeam: {
-      team: "Houston",
-      points: 95,
-      isWinner: true,
-    },
-  },
-  {
-    awayTeam: {
-      team: "Golden State",
-      points: 94,
-      isWinner: false,
-    },
-    homeTeam: {
-      team: "Houston",
-      points: 98,
-      isWinner: true,
-    },
-  },
-  {
-    homeTeam: {
-      team: "Golden State",
-      points: 115,
-      isWinner: true,
-    },
-    awayTeam: {
-      team: "Houston",
-      points: 86,
-      isWinner: false,
-    },
-  },
-  {
-    awayTeam: {
-      team: "Golden State",
-      points: 101,
-      isWinner: true,
-    },
-    homeTeam: {
-      team: "Houston",
-      points: 92,
-      isWinner: false,
-    },
-  },
+	{
+		awayTeam : {
+			team     : 'Golden State',
+			points   : 119,
+			isWinner : true
+		},
+		homeTeam : {
+			team     : 'Houston',
+			points   : 106,
+			isWinner : false
+		}
+	},
+	{
+		awayTeam : {
+			team     : 'Golden State',
+			points   : 105,
+			isWinner : false
+		},
+		homeTeam : {
+			team     : 'Houston',
+			points   : 127,
+			isWinner : true
+		}
+	},
+	{
+		homeTeam : {
+			team     : 'Golden State',
+			points   : 126,
+			isWinner : true
+		},
+		awayTeam : {
+			team     : 'Houston',
+			points   : 85,
+			isWinner : false
+		}
+	},
+	{
+		homeTeam : {
+			team     : 'Golden State',
+			points   : 92,
+			isWinner : false
+		},
+		awayTeam : {
+			team     : 'Houston',
+			points   : 95,
+			isWinner : true
+		}
+	},
+	{
+		awayTeam : {
+			team     : 'Golden State',
+			points   : 94,
+			isWinner : false
+		},
+		homeTeam : {
+			team     : 'Houston',
+			points   : 98,
+			isWinner : true
+		}
+	},
+	{
+		homeTeam : {
+			team     : 'Golden State',
+			points   : 115,
+			isWinner : true
+		},
+		awayTeam : {
+			team     : 'Houston',
+			points   : 86,
+			isWinner : false
+		}
+	},
+	{
+		awayTeam : {
+			team     : 'Golden State',
+			points   : 101,
+			isWinner : true
+		},
+		homeTeam : {
+			team     : 'Houston',
+			points   : 92,
+			isWinner : false
+		}
+	}
 ];
 
-// Create container
-const ulParent = document.createElement("ul");
+// **************************************************
+// STEP 2 - Refactored & Re-usable
+// **************************************************
 
-// iterate through data
-for (let game of warriorsGames) {
-  // Create element
-  const gameLi = document.createElement("li");
-  // Access elements
-  const { awayTeam, homeTeam } = game;
-  const { team: aTeam, points: aPoints } = awayTeam; // Rename team (from awayTeam of game) by aTeam
-  const { team: hTeam, points: hPoints } = homeTeam;
-  const teamNames = `${aTeam} @ ${hTeam}`;
-  // Compare the scores
-  let scores;
-  if (aPoints > hPoints) {
-    scores = `<b>${aPoints}</b>-${hPoints}`;
-  } else {
-    scores = `${aPoints}-<b>${hPoints}</b>`;
-  }
-  // Check where we have Golden State as a team
-  const warriors = aTeam === "Golden State" ? awayTeam : homeTeam; // warriors is a reference to each object
-  // Add the corresponding class
-  gameLi.classList.add(warriors.isWinner ? "win" : "lost"); // Add the corresponding class depending on isWinner
-  gameLi.innerHTML = `${teamNames} ${scores}`; // Add the content into the "li"
-  ulParent.append(gameLi); // Add the liste "gameLi" into the "ul"
-}
+const makeChart = (games, targetTeam) => {
+	const ulParent = document.createElement('ul');
+	for (let game of games) {
+		const gameLi = document.createElement('li');
+		gameLi.innerHTML = getScoreLine(game);
+		gameLi.classList.add(isWinner(game, targetTeam) ? 'win' : 'loss');
+		ulParent.appendChild(gameLi);
+	}
+	return ulParent;
+};
 
-document.body.append(ulParent); // Add the "ul" into the body
+const isWinner = ({ homeTeam, awayTeam }, targetTeam) => {
+	const target = homeTeam.team === targetTeam ? homeTeam : awayTeam;
+	return target.isWinner;
+};
+
+const getScoreLine = ({ homeTeam, awayTeam }) => {
+	const { team: hTeam, points: hPoints } = homeTeam;
+	const { team: aTeam, points: aPoints } = awayTeam;
+	const teamNames = `${aTeam} @ ${hTeam}`;
+	let scoreLine;
+	if (aPoints > hPoints) {
+		scoreLine = `<b>${aPoints}</b>-${hPoints}`;
+	}
+	else {
+		scoreLine = `${aPoints}-<b>${hPoints}</b>`;
+	}
+	return `${teamNames} ${scoreLine}`;
+};
+
+//Select the 2 sections to append to (from index.html)
+const gsSection = document.querySelector('#gs');
+const houstonSection = document.querySelector('#hr');
+
+// Make the 2 charts:
+const gsChart = makeChart(warriorsGames, 'Golden State');
+const hrChart = makeChart(warriorsGames, 'Houston');
+
+//Append them!
+gsSection.appendChild(gsChart);
+houstonSection.appendChild(hrChart);
